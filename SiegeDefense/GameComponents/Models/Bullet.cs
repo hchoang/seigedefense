daea04bee128fb6a86bc9fbc6c376f11170f005c
Huy Cat Hoang <cat.hoanghuy@gmail.com>
2016-08-23 17:30:44 +1000

    add bullet model

---
+++ SiegeDefense/GameComponents/Models/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace SiegeDefense.GameComponents.Models
+{
+    class Bullet : BaseModel
+    {
+        public Bullet(Model model, Vector3 Position): base(model, Position)
+        {
+            ScaleMatrix = Matrix.CreateScale(1);
+        }
+    }
+}
