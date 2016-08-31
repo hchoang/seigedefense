b2177c28dde15ad852ddb19dbaf9b53a88ea3fef
Huy Cat, Hoang <cat.hoanghuy@gmail.com>
2016-08-31 16:24:15 +1000

    Added game detail on screeen

---
+++ SiegeDefense/GameScreens/GameDetailSprite.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SiegeDefense.GameComponents;
+using System;
+
+namespace SiegeDefense.GameScreens
+{
+    class GameDetailSprite : _2DSprite
+    {
+        private SpriteBatch spriteBatch;
+
+        private SpriteFont font;
+        private Vector2 position;
+        private Color color;
+        private String text;
+
+        public GameDetailSprite(SpriteFont font, String text, Vector2 position, Color color)
+        {
+            spriteBatch = Game.Services.GetService<SpriteBatch>();
+
+            this.font = font;
+            this.position = position;
+            this.color = color;
+            this.text = text;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, text, position, color);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
--- SiegeDefense/GameScreens/MainGameScreen.cs
+++ SiegeDefense/GameScreens/MainGameScreen.cs
@@ -18,6 +18,8 @@ namespace SiegeDefense.GameScreens {
         private SoundBankManager soundManager;
 
         private ModelManager modelManager;
+        private GameDetailSprite pointSprite;
+        private GameDetailSprite bloodSprite;
 
 
         public MainGameScreen() {
@@ -25,16 +27,21 @@ namespace SiegeDefense.GameScreens {
             skyBox = new Skybox();
             modelManager = new ModelManager();
             soundManager = new SoundBankManager();
+            pointSprite = new GameDetailSprite(Game.Content.Load<SpriteFont>(@"Fonts\Arial"), "Point: 0", new Vector2(50, 50), Color.Green);
+            bloodSprite = new GameDetailSprite(Game.Content.Load<SpriteFont>(@"Fonts\Arial"), "Blood: 100", new Vector2(50, 100), Color.Green);
 
             map = new MultiTexturedHeightMap(10, 200);
             Vector3 cameraPosition = new Vector3(500, 0, 500);
             cameraPosition = new Vector3(cameraPosition.X, map.GetHeight(cameraPosition), cameraPosition.Z);
             //camera = new FPSCamera(cameraPosition, new Vector3(100, 20, 100), Vector3.Up);
-
+            
             Game.Components.Add(skyBox);
             Game.Components.Add(map);
             Game.Components.Add(modelManager);
             Game.Components.Add(soundManager);
+            Game.Components.Add(pointSprite);
+            Game.Components.Add(bloodSprite);
+
 
             //Vector3 position = new Vector3(100, 0, 100);
             //Matrix test = Matrix.CreateWorld(position, Vector3.Normalize(new Vector3(10, 0, 10)), Vector3.Up);
